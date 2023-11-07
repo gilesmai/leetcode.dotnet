3eaf4d28b4896531debd792cf762089d56f5174c
Zhoubao Mai <mzb429@gmail.com>
2023-11-08 09:25:32 +1100

    49 Group Anagrams

---
+++ leetcode/Arrays&Hashing/Group Anagrams.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays_Hashing
+{
+    internal class Group_Anagrams
+    {
+        public class Solution
+        {
+            public IList<IList<string>> GroupAnagrams(string[] strs)
+            {
+                Dictionary<string, IList<string>> anagrams = new Dictionary<string, IList<string>>();
+                foreach (string str in strs)
+                {
+                    char[] chars = str.ToCharArray();
+                    Array.Sort(chars);
+                    string sortedStr = new String(chars);
+                    if (anagrams.TryGetValue(sortedStr, out var list))
+                    {
+                        list.Add(str);
+                    }
+                    else
+                    {
+                        anagrams.Add(sortedStr, new List<string> { str });
+                    }
+                }
+                return anagrams.Values.ToList();
+            }
+        }
+    }
+}
