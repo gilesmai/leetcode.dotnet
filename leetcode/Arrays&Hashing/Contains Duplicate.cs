793ea83500824fd2c0a8cb00e34e1155f1affa03
Zhoubao Mai <mzb429@gmail.com>
2023-11-07 18:58:59 +1100

    217 Contains Duplicate

---
+++ leetcode/Arrays&Hashing/Contains Duplicate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays_Hashing
+{
+    internal class Contains_Duplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            return set.Count != nums.Count();
+        }
+    }
+}
