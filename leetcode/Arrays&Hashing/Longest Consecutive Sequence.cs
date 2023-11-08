27ea5453f47d4557aa3ebb5f8ae51c43366ecb3d
Zhoubao Mai <mzb429@gmail.com>
2023-11-08 16:35:01 +1100

    128. longest consecutive sequence

---
+++ leetcode/Arrays&Hashing/Longest Consecutive Sequence.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.Arrays_Hashing
+{
+    internal class Longest_Consecutive_Sequence
+    {
+        //Hashing
+        //public int LongestConsecutive(int[] nums)
+        //{
+        //    var hashNums = new HashSet<int>(nums);
+        //    int longestConsecutive = 0;
+        //    foreach (int i in hashNums)
+        //    {
+        //        if (!hashNums.Contains(i - 1))
+        //        {
+        //            int temp = i + 1;
+        //            while (hashNums.Contains(temp))
+        //            {
+        //                temp += 1;
+        //            }
+        //            longestConsecutive = Math.Max(longestConsecutive, temp - i);
+        //            if (longestConsecutive > hashNums.Count / 2)
+        //            {
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    return longestConsecutive;
+        //}
+
+        //DP
+        public int LongestConsecutive(int[] nums)
+        {
+            int DP(int num, Dictionary<int, int> lookUp)
+            {
+                if (!lookUp.ContainsKey(num))
+                {
+                    return 0;
+                }
+                if (lookUp[num] != 0)
+                {
+                    return lookUp[num];
+                }
+                lookUp[num] = 1 + DP(num + 1, lookUp);
+                return lookUp[num];
+            }
+            Dictionary<int, int> lookUp = new HashSet<int>(nums).ToDictionary(x => x, x => 0);
+            foreach (int num in lookUp.Keys)
+            {
+                DP(num, lookUp);
+            }
+            return lookUp.Values.Count > 0 ? lookUp.Values.Max() : 0;
+        }
+    }
+}
